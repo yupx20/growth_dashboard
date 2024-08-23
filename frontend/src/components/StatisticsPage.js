16153520b2c39133f8a6b6cdd89ba4226af4f847
rizalyu <yudianto.r78@gmail.com>
2024-08-23 10:35:29 +0700

    update statistics page

--- frontend/src/components/StatisticsPage.js
+++ frontend/src/components/StatisticsPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Chart, ArcElement } from 'chart.js';
-import { Pie } from 'react-chartjs-2';
+import { Chart, ArcElement, ChartJS, BarElement, CategoryScale, LinearScale, Tooltip, Legend } from 'chart.js';
+import { Pie, Bar } from 'react-chartjs-2';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,7 +8,7 @@ import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import '../styling/Statistics.css';
 
 // Daftarkan elemen 'arc'
-Chart.register(ArcElement);
+Chart.register(ArcElement,CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
 const StatisticsPage = () => {
   const navigate = useNavigate();
@@ -17,7 +17,7 @@ const StatisticsPage = () => {
     navigate(-1); // Go back to the previous page
   };
 
-  const data = {
+  const data1 = {
     labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
     datasets: [
       {
@@ -44,12 +44,46 @@ const StatisticsPage = () => {
     ],
   };
 
+  const data2 = {
+    labels: ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni'],
+    datasets: [
+      {
+        label: 'Pendapatan 2023 (dalam juta)',
+        data: [12, 19, 8, 15, 22, 30],
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+      {
+        label: 'Pendapatan 2022 (dalam juta)',
+        data: [10, 17, 6, 13, 20, 25],
+        backgroundColor: 'rgba(153, 102, 255, 0.6)',
+        borderColor: 'rgba(153, 102, 255, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Perbandingan Pendapatan Semester 1 Tahun 2022 vs 2023',
+      },
+    },
+  };
+
   return (
     <div className="statistics-container">
       <Navbar />
+      <div className="chart-container">
       <h2>Statistics</h2>
-      <div className="pie-chart-container">
-        <Pie data={data} />
+        <Pie data={data1} />
+      </div>
+      <div className="chart-container">
+      <h2>Bar Chart</h2>
+        <Bar data={data2} options={options} />
       </div>
       <button className="back-button" onClick={handleBackClick}>
           <FontAwesomeIcon icon={faArrowLeft} />
